cefcf61c72206f94f3a6cab0973ffddfbf4255e7
mariashakleva <maria.sakleva@gmail.com>
2019-01-16 18:07:18 +0100

    working on editing and updating idea

--- resources/js/components/BBIdeasList.vue
+++ resources/js/components/BBIdeasList.vue
@@ -5,7 +5,7 @@
         </div>
         <div class="row d-flex bg-lightgray p-3 pt-lg-5 mt-4 rounded gray-border justify-content-around">
             <div v-for="idea in ideas" :key="idea.id">
-                <bb-idea :idea="idea" :boardId="board.id"></bb-idea>
+                <bb-idea :idea="idea" :unique="idea.id" :boardId="board.id"></bb-idea>
             </div>
         </div>
     </div>
@@ -19,15 +19,21 @@
         data() {
             return {
                 ideas: this.boardIdeas,
-                idea: {
-                    'board_id': this.board.id
-                }
+                idea: {}
             }
+        },
+
+        mounted() {
+            Event.$on('update', () => this.updateBoard());
         },
 
         methods: {
             createIdea() {
                 this.ideas.unshift(this.idea);
+            },
+            updateBoard(val) {
+                console.log(val);
+                // this.ideas[0] = val;
             }
         }
     }
